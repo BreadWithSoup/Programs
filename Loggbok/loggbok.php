0673a823f623f07052f08c04ad715553be3b0585
BreadWithSoup <erik.post@elev.ga.ntig.se>
2022-09-02 11:58:55 +0200

    Går ej längre att lännga upp samma inlägg flera

--- Loggbok/loggbok.php
+++ Loggbok/loggbok.php
@@ -1,11 +1,19 @@
 <?php
     require_once ("db.php");
 
+    $Datum = "0000-00-00";
+    $TidStart = "00:00:00";
+    $TidSlut = "00:00:00";
+    $Händelse = NULL;
+    $Kommentar = NULL;
+    $Same = FALSE;
+
     if (!empty($_POST)){
-        $Kommentar=$_POST['Kommentar'];
-        $Datum=$_POST['Datum'];
-        echo $Kommentar;
-        echo $Datum;
+        $Datum = $_POST['Datum'];
+        $TidStart = $_POST['TidStart'] .":00";
+        $TidSlut = $_POST['TidSlut'] .":00";
+        $Händelse = $_POST['Händelse'];
+        $Kommentar = $_POST['Kommentar'];
     }
     $sql = "SELECT * FROM `logg`";
     $result = $conn->query($sql);
@@ -13,19 +21,24 @@
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["Datum"]. " " . $row["Tid"]. "<br>";
+            if (($Datum == $row['Datum']) && ($TidStart == $row['TidStart']) && ($TidSlut == $row['TidSlut']) && ($Händelse == $row['Händelse']) && ($Kommentar == $row['Kommentar'])){
+                $Same = TRUE;
+            }
+
+            echo "id: " .$row['id'] ." - Datum: " .$row['Datum'] ." - TidStart: " .$row['TidStart'] ." - TidSlut: " .$row['TidSlut'] ." - Händelse: " .$row['Händelse'] ." - Kommentar: " .$row['Kommentar'] ."<br>";
         }
     } 
     else {
         echo "0 results";
     }
-    if $sql != $logg{
-        $sql = "INSERT INTO `logg` (`id`, `Datum`, `Tid`, `Händelse`, `Kommentar`) VALUES (NULL, '$Datum', '13:22', 'fixar insert data', '$Kommentar')";
+
+    if ($Same == FALSE){
+        $sql = "INSERT INTO `logg` (`id`, `Datum`, `TidStart`, `TidSlut`, `Händelse`, `Kommentar`) VALUES (NULL, '$Datum', '$TidStart', '$TidSlut', '$Händelse', '$Kommentar')";
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
         } 
         else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            echo "Error: " .$sql ."<br>" .$conn->error;
         }
     }
 ?>
@@ -40,13 +53,7 @@
 <body>
     <div id=leftbox>
         <h1>Månad</h1>
-        <div class="box1" id="box1a"><p>1</p>
-            <form method="post">
-                Kommentar: <input type="text" name="Kommentar" required="require"><br>
-                Datum: <input type="date" name="Datum"><br>
-                <input type="submit">
-            </form>
-        </div>
+        <div class="box1" id="box1a"><p>1</p></div>
         <div class="box1" id="box1b"><p>2</p></div>
         <div class="box1" id="box1c"><p>3</p></div>
         <div class="box1" id="box1d"><p>4</p></div>
@@ -54,7 +61,18 @@
         <div class="box1" id="box1f"><p>6</p></div>
     </div>
     <div id="rightbox">
-        <a href="loggbok1.php"><div class="box2" id="box2a"></div></a>
+        <!-- <a href="loggbok1.php"> -->
+            <div class="box2" id="box2a">
+                <form method="post">
+                    Datum: <input type="date" name="Datum" required="require"><br>
+                    TidStart: <input type="time" name="TidStart" required="require"><br>
+                    TidSlut: <input type="time" name="TidSlut" required="require"><br>
+                    Händelse: <input type="text" name="Händelse" required="require"><br>
+                    Kommentar: <input type="text" name="Kommentar" required="require"><br>
+                    <input type="submit">
+                </form>
+            </div>
+        <!-- </a> -->
         <a href="loggbok2.php"><div class="box2" id="box2b"></div></a>
         <a href="loggbok3.php"><div class="box2" id="box2c"></div></a>
         <a href="loggbok4.php"><div class="box2" id="box2d"></div></a>
